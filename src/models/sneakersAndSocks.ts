e2def7cbf62b0d853fbcac632b9aa09eba2f20c6
Roberto <cacaracteres86@gmail.com>
2022-11-11 19:56:09 +0100

    Models Types

---
+++ src/models/sneakersAndSocks.ts
@@ -0,0 +1,10 @@
+export type sneakersAndSocks = {
+    id: number;
+    name: string;
+    offer: boolean;
+    price: number;
+    brand: string;
+    color: string;
+    image: string;
+    Description: string;
+};
